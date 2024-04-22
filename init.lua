04bf775d1b9e417a223e43f88a54bb187a16ec35
nkuek <nkuek1@gmail.com>
2024-04-22 16:07:11 -0400

    respect project prettier config

--- init.lua
+++ init.lua
@@ -567,9 +567,54 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        javascript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        json = { 'prettier' },
+        html = { 'prettier' },
+        css = { 'prettier' },
       },
     },
+    config = function(_, opts)
+      local conform = require 'conform'
+      conform.setup(opts)
+      -- Customize prettier args
+      require('conform.formatters.prettier').args = function(_, ctx)
+        local prettier_roots = { '.prettierrc', '.prettierrc.json', 'prettier.config.js' }
+        local args = { '--stdin-filepath', '$FILENAME' }
+
+        local localPrettierConfig = vim.fs.find(prettier_roots, {
+          upward = true,
+          path = ctx.dirname,
+          type = 'file',
+        })[1]
+        local globalPrettierConfig = vim.fs.find(prettier_roots, {
+          path = vim.fn.stdpath 'config',
+          type = 'file',
+        })[1]
+        local disableGlobalPrettierConfig = os.getenv 'DISABLE_GLOBAL_PRETTIER_CONFIG'
+
+        -- Project config takes precedence over global config
+        if localPrettierConfig then
+          vim.list_extend(args, { '--config', localPrettierConfig })
+        elseif globalPrettierConfig and not disableGlobalPrettierConfig then
+          vim.list_extend(args, { '--config', globalPrettierConfig })
+        end
+
+        local hasTailwindPrettierPlugin = vim.fs.find('node_modules/prettier-plugin-tailwindcss', {
+          upward = true,
+          path = ctx.dirname,
+          type = 'directory',
+        })[1]
+
+        if hasTailwindPrettierPlugin then
+          vim.list_extend(args, { '--plugin', 'prettier-plugin-tailwindcss' })
+        end
+
+        return args
+      end
+    end,
   },
 
   { -- Autocompletion
