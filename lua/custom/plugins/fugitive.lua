341fab6346808b2266746c8dd617e049096d36f1
nkuek <nkuek1@gmail.com>
2024-04-23 01:05:30 -0400

    update fugitive descriptions

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -7,8 +7,8 @@ return {
     vim.keymap.set('n', '<leader>gl', ':G log<CR>', { noremap = true, desc = '[G]it [l]og' })
     vim.keymap.set('n', '<leader>gd', ':G diff<CR>', { noremap = true, desc = '[G]it [d]iff' })
     vim.keymap.set('n', '<leader>gc', ':G commit -m ', { noremap = true, desc = '[G]it [c]ommit' })
-    vim.keymap.set('n', '<leader>gp', ':G push<CR>', { noremap = true, desc = '[G]it [P]ush' })
-    vim.keymap.set('n', '<leader>gP', ':G pull --rebase<CR>', { noremap = true, desc = '[G]it [p]ull' })
+    vim.keymap.set('n', '<leader>gp', ':G push<CR>', { noremap = true, desc = '[G]it [p]ush' })
+    vim.keymap.set('n', '<leader>gP', ':G pull --rebase<CR>', { noremap = true, desc = '[G]it [P]ull' })
     vim.keymap.set('n', '<leader>gC', ":G ac '", { noremap = true, desc = '[G]it [A]dd and [C]ommit' })
   end,
 }
