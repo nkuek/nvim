9e96d11983488d30bf3c35b7d472cdb1cfc04382
nkuek <nkuek1@gmail.com>
2024-05-02 18:35:02 -0400

    better filtering for neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -22,6 +22,17 @@ return {
       },
       filtered_items = {
         hide_dotfiles = false,
+        hide_gitignore = false,
+        hide_hidden = false,
+        hide_by_name = {
+          'node_modules',
+          '.git',
+          '.DS_Store',
+        },
+        always_show = {
+          '.env.local',
+          '.env',
+        },
       },
     },
   },
