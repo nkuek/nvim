9e39df3ad679c76be22bce52e122ccc30e8e16b8
nkuek <nkuek1@gmail.com>
2024-08-06 14:04:04 -0400

    fix whichkey deprecated options warnings

--- init.lua
+++ init.lua
@@ -237,24 +237,38 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it Fugitive', _ = 'which_key_ignore' },
-        ['<leader>l'] = { name = '[L]azy', _ = 'which_key_ignore' },
-        ['<leader><leader>'] = { name = 'Apply changes', _ = 'which_key_ignore' },
-        ['<leader>a'] = { name = '[A]dd to Harpoon', _ = 'which_key_ignore' },
-        ['<leader>Y'] = { name = 'Hard [Y]ank to +clipboard', _ = 'which_key_ignore' },
-        ['<leader>y'] = { name = 'Soft [y]ank to +clipboard', _ = 'which_key_ignore' },
+      require('which-key').add {
+        {
+          { '<leader><leader>', group = 'Apply changes' },
+          { '<leader><leader>_', hidden = true },
+          { '<leader>Y', group = 'Hard [Y]ank to +clipboard' },
+          { '<leader>Y_', hidden = true },
+          { '<leader>a', group = '[A]dd to Harpoon' },
+          { '<leader>a_', hidden = true },
+          { '<leader>c', group = '[C]ode' },
+          { '<leader>c_', hidden = true },
+          { '<leader>d', group = '[D]ocument' },
+          { '<leader>d_', hidden = true },
+          { '<leader>g', group = '[G]it Fugitive' },
+          { '<leader>g_', hidden = true },
+          { '<leader>h', group = 'Git [H]unk' },
+          { '<leader>h_', hidden = true },
+          { '<leader>l', group = '[L]azy' },
+          { '<leader>l_', hidden = true },
+          { '<leader>r', group = '[R]ename' },
+          { '<leader>r_', hidden = true },
+          { '<leader>s', group = '[S]earch' },
+          { '<leader>s_', hidden = true },
+          { '<leader>w', group = '[W]orkspace' },
+          { '<leader>w_', hidden = true },
+          { '<leader>y', group = 'Soft [y]ank to +clipboard' },
+          { '<leader>y_', hidden = true },
+        },
       }
       -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
+      require('which-key').add {
+        { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
+      }
     end,
   },
 
